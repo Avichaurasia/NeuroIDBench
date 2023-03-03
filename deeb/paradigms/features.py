263adbe3edd522ca1a36209c89617d0060fd3580
Avichaurasia <avichaurasia1994@gmail.com>
2023-03-03 08:36:11 +0100

    Modified the average_band_power function

--- deeb/paradigms/features.py
+++ deeb/paradigms/features.py
@@ -17,16 +17,21 @@ import pandas as pd
 from collections import OrderedDict
 from itertools import islice
 import logging
+from tqdm import tqdm
+#import multiprocessing
+from mne.utils import _url_to_local_path, verbose
+from deeb.paradigms.psd_utils import computing_psd
+
 #from deeb.paradigms.base import BaseParadigm
 log = logging.getLogger(__name__)
 
 class Features():
 
+    
     def auto_regressive_coeffecients(self, subject_dict):
-        print("in Ar")
         df_list = []
         order = 6
-        for subject, sessions in subject_dict.items():
+        for subject, sessions in tqdm(subject_dict.items(), desc="Computing AR Coeffecients"):
             for session, runs in sessions.items():
                 for run, epochs in runs.items():
                     epochs_data = epochs.get_data()
@@ -42,8 +47,18 @@ class Features():
         df = pd.DataFrame(df_list)
         return df
 
+    def computing_psd(self, epochs):
+        tmax=epochs.tmax
+        tmin=epochs.tmin
+        sfreq=epochs.info['sfreq']
+
+        # Using mne.Epochs in-built method compute_psd to calculate PSD using welch's method
+        spectrum=epochs.compute_psd(method="welch", n_fft=int(sfreq * (tmax - tmin)),
+            n_overlap=0, n_per_seg=None, fmin=1, fmax=50, tmin=tmin, tmax=tmax, verbose=False)
+
+        return spectrum.get_data(return_freqs=True)
+    
     def average_band_power(self, subject_dict):
-        print("in average band power")
         df_psd=pd.DataFrame()
         FREQ_BANDS = {"delta" : [1,4],
                         "theta" : [4,8],
@@ -51,118 +66,134 @@ class Features():
                         "beta" : [12,30],
                         "gamma" : [30, 50]}
         
+        #pool = multiprocessing.Pool()
+        results = []
+
         for subject, sessions in subject_dict.items():
             for session, runs in sessions.items():
                 for run, epochs in runs.items():
-                    tmax=epochs.tmax
-                    tmin=epochs.tmin
-                    sfreq=epochs.info['sfreq']
-                    # specific frequency bands
-                    spectrum=epochs.compute_psd(method="welch", n_fft=int(sfreq * (tmax - tmin)),
-                        n_overlap=0, n_per_seg=None, fmin=1, fmax=50, tmin=tmin, tmax=tmax, verbose=False)
+                    result = self.computing_psd(epochs)
+                    results.append((result, subject, epochs))
 
-                    psds, freqs=spectrum.get_data(return_freqs=True)
-                    for i in range(len(psds)):
-                        features={}
-                        #event_id = list(epochs.event_id.values())[0]
-                        #features = {'Subject': subject, 'Event_id_PSD': event_id}
-                        for j in range(len(psds[i])):
-                            welch_psd=psds[i][j]
-                            X=[]
-                            for fmin, fmax in FREQ_BANDS.values():
-                                psds_band=welch_psd[(freqs >= fmin) & (freqs < fmax)].mean()
-                                X.append(psds_band)
+        for result, subject, epochs in tqdm(results, desc="Computing PSD"):
+            psds, freqs = result
+            for i in range(len(psds)):
+                features={}
+                for j in range(len(psds[i])):
+                    welch_psd=psds[i][j]
+                    X=[]
+                    for fmin, fmax in FREQ_BANDS.values():
+                        psds_band=welch_psd[(freqs >= fmin) & (freqs < fmax)].mean()
+                        X.append(psds_band)
             
-                            #features['Subject']=subject
-                            #features['Event_id_PSD']=list(epochs[i].event_id.values())[0]
-        
-                            channel=epochs.ch_names[j]
-                            for d in range(len(X)):
-                                band_name=[*FREQ_BANDS][d]
-                                colum_name=channel+"-"+band_name
-                                features[colum_name]=X[d]
-                        data_step = [features]
-                        df_psd=df_psd.append(data_step,ignore_index=True)
+                    channel=epochs.ch_names[j]
+                    for d in range(len(X)):
+                        band_name=[*FREQ_BANDS][d]
+                        colum_name=channel+"-"+band_name
+                        features[colum_name]=X[d]
+                data_step = [features]
+                df_psd=df_psd.append(data_step,ignore_index=True)
 
         return df_psd
+
+    # def average_band_power(self, subject_dict):
+    #     df_psd=pd.DataFrame()
+    #     FREQ_BANDS = {"delta" : [1,4],
+    #                     "theta" : [4,8],
+    #                     "alpha" : [8, 12],
+    #                     "beta" : [12,30],
+    #                     "gamma" : [30, 50]}
+        
+    #     for subject, sessions in tqdm(subject_dict.items(), desc="Computing PSD"):
+    #         for session, runs in sessions.items():
+    #             for run, epochs in runs.items():
+    #                 tmax=epochs.tmax
+    #                 tmin=epochs.tmin
+    #                 sfreq=epochs.info['sfreq']
+
+    #                 # Using mne.Epochs in-built method compute_psd to calculate PSD using welch's method
+    #                 spectrum=epochs.compute_psd(method="welch", n_fft=int(sfreq * (tmax - tmin)),
+    #                     n_overlap=0, n_per_seg=None, fmin=1, fmax=50, tmin=tmin, tmax=tmax, verbose=False)
+
+    #                 psds, freqs=spectrum.get_data(return_freqs=True)
+    #                 for i in range(len(psds)):
+    #                     features={}
+    #                     #event_id = list(epochs.event_id.values())[0]
+    #                     #features = {'Subject': subject, 'Event_id_PSD': event_id}
+    #                     for j in range(len(psds[i])):
+    #                         welch_psd=psds[i][j]
+    #                         X=[]
+    #                         for fmin, fmax in FREQ_BANDS.values():
+    #                             psds_band=welch_psd[(freqs >= fmin) & (freqs < fmax)].mean()
+    #                             X.append(psds_band)
+            
+    #                         #features['Subject']=subject
+    #                         #features['Event_id_PSD']=list(epochs[i].event_id.values())[0]
+        
+    #                         channel=epochs.ch_names[j]
+    #                         for d in range(len(X)):
+    #                             band_name=[*FREQ_BANDS][d]
+    #                             colum_name=channel+"-"+band_name
+    #                             features[colum_name]=X[d]
+    #                     data_step = [features]
+    #                     df_psd=df_psd.append(data_step,ignore_index=True)
+
+    #     return df_psd
     
     def extract_features(self, dataset, subject_dict, labels):
         df=pd.DataFrame()
         df_AR=self.auto_regressive_coeffecients(subject_dict)
         df_PSD=self.average_band_power(subject_dict)
         df=pd.concat([df_AR,df_PSD], axis=1)
-        print(df.head())
+        #print(df.head())
         return df
-  
     
-    # def AR(subject_dict=None):
-    #     print("in auto_regressive_coeffecients")
-    #     df=pd.DataFrame()
-    #     for subject, sessions in subject_dict.items():
-    #         for session, runs in sessions.items():
-    #             for run, epochs in runs.items():
-    #                 #epochs=data
-    #                 epochs_data=epochs.get_data()
-    #                 order=6
-    #                 for i in range(len(epochs_data)):
-    #                     dictemp={}
-    #                     for j in range(len(epochs_data[i])):
-    #                         rho, sigma = sm.regression.yule_walker(epochs_data[i][j], order=order, method="mle")
-    #                         dictemp['Subject']=subject
-    #                         dictemp['Event_id']=list(epochs[i].event_id.values())[0]
-        
-    #                         first=epochs.ch_names[j]
-    #                         for d in range(order):
-    #                             colum_name=first+"-AR"+str(d+1)
-    #                             dictemp[colum_name]=rho[d]  
-    #                     data_step = [dictemp]
-    #                     df=df.append(data_step,ignore_index=True)
-    #     return df
+    #import multiprocessing
 
-
-    # def PSD(subject_dict):
-    #     print("in PSD")
-
-    #     df_psd = pd.DataFrame()
-    #     # specific frequency bands
+    # def average_band_power(self, subject_dict):
+    #     df_psd=pd.DataFrame()
     #     FREQ_BANDS = {"delta" : [1,4],
-    #                 "theta" : [4,8],
-    #                 "alpha" : [8, 12],
-    #                 "beta" : [12,30],
-    #                 "gamma" : [30, 50]}
+    #                     "theta" : [4,8],
+    #                     "alpha" : [8, 12],
+    #                     "beta" : [12,30],
+    #                     "gamma" : [30, 50]}
         
+    #     pool = multiprocessing.Pool()
+    #     results = []
+
     #     for subject, sessions in subject_dict.items():
     #         for session, runs in sessions.items():
     #             for run, epochs in runs.items():
-    #                 #epochs=subject_dict[subject]
-    #                 tmax = epochs.tmax
-    #                 tmin = epochs.tmin
-    #                 sfreq = epochs.info['sfreq']
-                    
-    #                 spectrum = epochs.compute_psd(method="welch", n_fft=int(sfreq * (tmax - tmin)),
-    #                                             n_overlap=0, n_per_seg=None, fmin=1, fmax=50, tmin=tmin, tmax=tmax, verbose=False)
-    #                 psds, freqs = spectrum.get_data(return_freqs=True)
-    #                 n_channels = len(psds[0])
-    #                 for i, event in enumerate(epochs):
-    #                     event_id = list(epochs.event_id.values())[0]
-    #                     features = {'Subject': subject, 'Event_id_PSD': event_id}
-    #                     for j in range(n_channels):
-    #                         welch_psd = psds[i][j]
-    #                         X = []
-    #                         for fmin, fmax in FREQ_BANDS.values():
-    #                             psds_band = welch_psd[(freqs >= fmin) & (freqs < fmax)].mean()
-    #                             X.append(psds_band)
+    #                 result = pool.apply_async(computing_psd, (epochs,))
+    #                 results.append((result, subject, epochs))
 
-    #                         channel = epochs.ch_names[j]
-    #                         for d in range(len(X)):
-    #                             band_name = [*FREQ_BANDS][d]
-    #                             column_name = channel + "-" + band_name
-    #                             features[column_name] = X[d]
-    #                     data_step = [features]
-    #                     df_psd = df_psd.append(data_step, ignore_index=True)
+    #     pool.close()
+    #     pool.join()
+
+    #     for result, subject, epochs in tqdm(results, desc="Computing PSD"):
+    #         psds, freqs = result.get()
+    #         for i in range(len(psds)):
+    #             features={}
+    #             for j in range(len(psds[i])):
+    #                 welch_psd=psds[i][j]
+    #                 X=[]
+    #                 for fmin, fmax in FREQ_BANDS.values():
+    #                     psds_band=welch_psd[(freqs >= fmin) & (freqs < fmax)].mean()
+    #                     X.append(psds_band)
+            
+    #                 channel=epochs.ch_names[j]
+    #                 for d in range(len(X)):
+    #                     band_name=[*FREQ_BANDS][d]
+    #                     colum_name=channel+"-"+band_name
+    #                     features[colum_name]=X[d]
+    #             data_step = [features]
+    #             df_psd=df_psd.append(data_step,ignore_index=True)
 
     #     return df_psd
 
+    
+    
+  
     
     
 
